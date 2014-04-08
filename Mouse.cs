d44b90a892c37f0c4f232940d8341ec97f073e06
Nova Nova <whitepoizon@gmail.com>
2014-04-08 02:04:26 +0200

    Initial commit

---
+++ Mouse.cs
@@ -0,0 +1,44 @@
+﻿namespace MouseControl {
+    public class Mouse {
+        public static void Move(int x, int y) {
+            NativeMethods.SetCursorPos(x, y);
+        }
+
+        public static void LeftClick(int x, int y) {
+            Move(x, y);
+            LeftDown();
+            LeftUp();
+        }
+
+        public static void LeftClick() {
+            LeftDown();
+            LeftUp();
+        }
+
+        public static void RightClick(int x, int y) {
+            Move(x, y);
+            RightDown();
+            RightUp();
+        }
+
+        public static void RightClick() {
+            RightDown();
+            RightUp();
+        }
+
+        public static void LeftDown() {
+            NativeMethods.mouse_event(NativeMethods.MouseEventFlags.Leftdown, 0u, 0u, 0u, 0);
+        }
+
+        public static void LeftUp() {
+            NativeMethods.mouse_event(NativeMethods.MouseEventFlags.Leftup, 0u, 0u, 0u, 0);
+        }
+
+        public static void RightDown() {
+            NativeMethods.mouse_event(NativeMethods.MouseEventFlags.Rightdown, 0u, 0u, 0u, 0);
+        }
+        public static void RightUp() {
+            NativeMethods.mouse_event(NativeMethods.MouseEventFlags.Rightup, 0u, 0u, 0u, 0);
+        }
+    }
+}
---
+++ NativeMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MouseControl {
+    internal class NativeMethods {
+        [Flags]
+        public enum MouseEventFlags : uint {
+            Leftdown = 2u,
+            Leftup = 4u,
+            Middledown = 32u,
+            Middleup = 64u,
+            Move = 1u,
+            Absolute = 32768u,
+            Rightdown = 8u,
+            Rightup = 16u,
+            Wheel = 2048u,
+            Xdown = 128u,
+            Xup = 256u
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(MouseEventFlags dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+    }
+}
